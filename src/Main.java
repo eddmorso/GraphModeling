3de707398a115484d3e3be8e95b2b0ccbef92579
Eduard <eddmorso@gmail.com>
2019-03-29 16:27:33 +0200

    add improvements

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
-import View.Figure;
 import View.Frame;
-
-import com.sun.xml.internal.messaging.saaj.soap.JpegDataContentHandler;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
 

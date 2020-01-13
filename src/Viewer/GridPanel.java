1f6d817a4037f4b07e940c5c748c68acb858f1d9
Eduard <eddmorso@gmail.com>
2020-01-13 21:13:11 +0200

    refactoring

--- src/Viewer/GridPanel.java
+++ src/Viewer/GridPanel.java
@@ -8,10 +8,14 @@ import java.awt.*;
 import java.util.List;
 
 public class GridPanel extends JPanel {
+
     @Override
     protected void paintComponent(Graphics g) {
         int width = (this.getWidth()) / 14;
         int height = (this.getHeight()) / 14;
+        List<Vertex> vertices = Graph.getInstance().getVertexList();
+        List<Connection> connections = Graph.getInstance().getConnectionList();
+
         Font font = new Font(Font.SANS_SERIF, Font.BOLD, 14);
 
         g.setFont(font);
@@ -19,68 +23,73 @@ public class GridPanel extends JPanel {
         g.fillRect(0,0,this.getWidth(), this.getHeight());
         g.setColor(Color.black);
 
-        for (int i = 0; i <= 14; i++){
-            for (int j = 0; j <= 14; j++){
-                g.drawLine(i * width,j * height,i * width,j * height);
+        for (int i = 0; i <= 14; i++) {
+            for (int j = 0; j <= 14; j++) {
+                g.drawLine(i * width, j * height, i * width, j * height);
             }
         }
-        List<Vertex> vertices = Graph.getInstance().getVertexList();
-        List<Connection> connections = Graph.getInstance().getConnectionList();
 
-        if (!connections.isEmpty()){
-            for (Connection connection : connections){
-                int initialX = connection.getStartVertex().getX() * width + width/2;
-                int initialY = connection.getStartVertex().getY() * height + height/2;
-                int endX = connection.getEndVertex().getX() * width + width/2;
-                int endY = connection.getEndVertex().getY() * height + height/2;
-
-                if (initialX == endX && initialY == endY){
-                    g.setColor(Color.red);
-                    g.drawOval(initialX - width, initialY - height, width, height);
-                }
-                g.setColor(Color.red);
-                g.drawLine(initialX, initialY, endX, endY);
-                g.setColor(Color.black);
-
-                if (initialX < endX && initialY < endY){
-                    g.drawString(String.valueOf(connection.getWeight()), initialX + (endX - initialX) / 2, initialY + (endY - initialY) / 2);
-                    g.fillOval(endX - width/2, endY - height/2, 5,5);
-                } else if (initialX < endX && initialY > endY){
-                    g.drawString(String.valueOf(connection.getWeight()), initialX + (endX - initialX) / 2, endY + (initialY - endY) / 2);
-                    g.fillOval(endX - width/2, endY + height/2, 5,5);
-                } else if (initialX > endX && initialY < endY){
-                    g.drawString(String.valueOf(connection.getWeight()), endX + (initialX - endX) / 2, initialY + (endY - initialY) / 2);
-                    g.fillOval(endX + width/2, endY - height/2, 5,5);
-                } else if (initialX > endX && initialY > endY) {
-                    g.drawString(String.valueOf(connection.getWeight()), endX + (initialX - endX) / 2, endY + (initialY - endY) / 2);
-                    g.fillOval(endX + width/2, endY + height/2, 5,5);
-                } else if (initialX == endX && initialY > endY){
-                    g.drawString(String.valueOf(connection.getWeight()), initialX, endY + (initialY - endY) / 2);
-                    g.fillOval(endX, endY + height/2, 5,5);
-                } else if (initialX == endX && initialY < endY){
-                    g.drawString(String.valueOf(connection.getWeight()), initialX, initialY + (endY - initialY) / 2);
-                    g.fillOval(endX, endY - (height/2 + 5), 5,5);
-                } else if (initialX > endX && initialY == endY){
-                    g.drawString(String.valueOf(connection.getWeight()), endX + (initialX - endX) / 2, initialY);
-                    g.fillOval(endX + width/2, endY, 5,5);
-                } else if (initialX < endX && initialY == endY){
-                    g.drawString(String.valueOf(connection.getWeight()), initialX + (endX - initialX) / 2, initialY);
-                    g.fillOval(endX - (width/2 + 5), endY, 5,5);
-                } else if (initialX == endX && initialY == endY){
-                    g.drawString(String.valueOf(connection.getWeight()), initialX - width, initialY - height);
-                }
-            }
+        if (!connections.isEmpty()) {
+            drawConnection(g, height, width, connections);
         }
         if (!vertices.isEmpty()) {
-            for (Vertex vertex : vertices) {
-                g.setColor(Color.black);
-                g.fillOval(vertex.getX() * width, vertex.getY() * height, width, height);
+            drawVertex(g, height, width, vertices);
+        }
+    }
 
-                g.setColor(Color.white);
-                g.drawString(String.valueOf(vertex.getWeight()),
-                        vertex.getX() * width + width/2 - 3, vertex.getY() * height + height/2);
-                g.drawString(String.valueOf(vertex.getId()),
-                        vertex.getX() * width + width/2 - 3, vertex.getY() * height + height - 5);
+    private void drawVertex(Graphics g, int height, int width, List<Vertex> vertices){
+        for (Vertex vertex : vertices) {
+            g.setColor(Color.black);
+            g.fillOval(vertex.getX() * width, vertex.getY() * height, width, height);
+            g.setColor(Color.white);
+            g.drawString(String.valueOf(vertex.getWeight()),
+                    vertex.getX() * width + width/2 - 3, vertex.getY() * height + height/2);
+            g.drawString(String.valueOf(vertex.getId()),
+                    vertex.getX() * width + width/2 - 3, vertex.getY() * height + height - 5);
+        }
+    }
+
+    private void drawConnection(Graphics g, int height, int width, List<Connection> connections) {
+        for (Connection connection : connections){
+            int initialX = connection.getStartVertex().getX() * width + width/2;
+            int initialY = connection.getStartVertex().getY() * height + height/2;
+            int endX = connection.getEndVertex().getX() * width + width/2;
+            int endY = connection.getEndVertex().getY() * height + height/2;
+
+            if (initialX == endX && initialY == endY){
+                g.setColor(Color.red);
+                g.drawOval(initialX - width, initialY - height, width, height);
+            }
+            g.setColor(Color.red);
+            g.drawLine(initialX, initialY, endX, endY);
+            g.setColor(Color.black);
+
+            if (initialX < endX && initialY < endY){
+                g.drawString(String.valueOf(connection.getWeight()), initialX + (endX - initialX) / 2, initialY + (endY - initialY) / 2);
+                g.fillOval(endX - width/2, endY - height/2, 5,5);
+            } else if (initialX < endX && initialY > endY){
+                g.drawString(String.valueOf(connection.getWeight()), initialX + (endX - initialX) / 2, endY + (initialY - endY) / 2);
+                g.fillOval(endX - width/2, endY + height/2, 5,5);
+            } else if (initialX > endX && initialY < endY){
+                g.drawString(String.valueOf(connection.getWeight()), endX + (initialX - endX) / 2, initialY + (endY - initialY) / 2);
+                g.fillOval(endX + width/2, endY - height/2, 5,5);
+            } else if (initialX > endX && initialY > endY) {
+                g.drawString(String.valueOf(connection.getWeight()), endX + (initialX - endX) / 2, endY + (initialY - endY) / 2);
+                g.fillOval(endX + width/2, endY + height/2, 5,5);
+            } else if (initialX == endX && initialY > endY){
+                g.drawString(String.valueOf(connection.getWeight()), initialX, endY + (initialY - endY) / 2);
+                g.fillOval(endX, endY + height/2, 5,5);
+            } else if (initialX == endX && initialY < endY){
+                g.drawString(String.valueOf(connection.getWeight()), initialX, initialY + (endY - initialY) / 2);
+                g.fillOval(endX, endY - (height/2 + 5), 5,5);
+            } else if (initialX > endX && initialY == endY){
+                g.drawString(String.valueOf(connection.getWeight()), endX + (initialX - endX) / 2, initialY);
+                g.fillOval(endX + width/2, endY, 5,5);
+            } else if (initialX < endX && initialY == endY){
+                g.drawString(String.valueOf(connection.getWeight()), initialX + (endX - initialX) / 2, initialY);
+                g.fillOval(endX - (width/2 + 5), endY, 5,5);
+            } else if (initialX == endX && initialY == endY){
+                g.drawString(String.valueOf(connection.getWeight()), initialX - width, initialY - height);
             }
         }
     }
